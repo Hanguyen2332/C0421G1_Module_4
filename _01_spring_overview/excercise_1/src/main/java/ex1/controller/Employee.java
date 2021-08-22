9ee0d364a6777d02f02c46043d7f4884cc3237bb
Hanguyen2332 <thanhha2332@gmail.com>
2021-08-22 11:59:42 +0700

    _01_spring_overview

---
+++ _01_spring_overview/excercise_1/src/main/java/ex1/controller/Employee.java
@@ -0,0 +1,12 @@
+package ex1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Employee {
+    @GetMapping("/employee") // localhost8080/employee
+    public String convert() {
+        return "index";
+    }
+}
